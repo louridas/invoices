bc1e47f74db7cb5cae67c1aa2af57a6b9c2de1bf
Panos Louridas <louridas@gmail.com>
2014-12-02 15:22:19 +0200

    add support for English

--- invoice.py
+++ invoice.py
@@ -50,6 +50,28 @@ one_to_twenty_f = [
     'δεκαεννέα'
     ]
 
+one_to_twenty_en = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen'
+    ]
+
 tens = [
     'δέκα',
     'είκοσι',
@@ -60,6 +82,18 @@ tens = [
     'εβδομήντα',
     'ογδόντα',
     'ενενήντα'
+    ]
+
+tens_en = [
+    'ten',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety'
     ]
 
 hundreds_n = [
@@ -86,94 +120,141 @@ hundreds_f = [
     'εννιακόσιες'
     ]
 
+hundreds_en = [ x + ' hundred' for x in one_to_twenty_en[0:10]]
+
 thousands = [
     'χίλια',
     'χιλιάδες'
     ]
+
+thousands_en = [
+    'one thousand',
+    'thousand'
+]
 
 millions = [
     'εκατομμύριο',
     'εκατομμύρια'
     ]
 
+millions_en = [
+    'one million',
+    'million'
+]
+
 billions = [
     'δισεκατομμύριο',
     'δισεκατομμύρια'
     ]
 
-def num_to_text_hundreds(number, f):
+billions_en = [
+    'one billion',
+    'billion'
+]
+
+def num_to_text_hundreds(number, f, english=False):
     parts = []
     h, mod100 = divmod(number, 100)
     t, mod10 = divmod(mod100, 10)
+    if english:
+        one_to_twenty_arr_f = one_to_twenty_en
+        one_to_twenty_arr_n = one_to_twenty_en
+        hundreds_arr_f = hundreds_en
+        hundreds_arr_n = hundreds_en
+        tens_arr = tens_en
+    else:
+        one_to_twenty_arr_f = one_to_twenty_f
+        one_to_twenty_arr_n = one_to_twenty_n
+        hundreds_arr_f = hundreds_f
+        hundreds_arr_n = hundreds_n
+        tens_arr = tens
     if h > 0:
-        if h == 1 and mod100 > 0:
-            parts.append(hundreds_n[h - 1] + 'ν')
+        if h == 1 and mod100 > 0 and english:
+            parts.append(hundreds_arr_n[h - 1] + 'ν')
         else:
             if f == True:
-                parts.append(hundreds_f[h - 1])
+                parts.append(hundreds_arr_f[h - 1])
             else:
-                parts.append(hundreds_n[h - 1])
+                parts.append(hundreds_arr_n[h - 1])
     if t > 1:
-        parts.append(tens[t - 1])
+        parts.append(tens_arr[t - 1])
         if mod10 > 0:
-            if f == True:
-                parts.append(one_to_twenty_f[mod10 - 1])
+            if english:
+                parts[-1] = parts[-1] + '-' + one_to_twenty_arr_f[mod10 - 1]
+            elif f == True:
+                parts.append(one_to_twenty_arr_f[mod10 - 1])
             else:
-                parts.append(one_to_twenty_n[mod10 - 1])
+                parts.append(one_to_twenty_arr_n[mod10 - 1])
     elif t == 1:
-        parts.append(one_to_twenty_n[10 + mod10 - 1])
+        parts.append(one_to_twenty_arr_n[10 + mod10 - 1])
     elif mod10 > 0:
         if f == True:
-            parts.append(one_to_twenty_f[mod10 - 1])
+            parts.append(one_to_twenty_arr_f[mod10 - 1])
         else:
-            parts.append(one_to_twenty_n[mod10 - 1])
+            parts.append(one_to_twenty_arr_n[mod10 - 1])
     return ' '.join(parts)
 
-def num_to_text_thousands(number):
+def num_to_text_thousands(number, english=False):
     th, r = divmod(number, 1000)
+    if english:
+        thousands_arr = thousands_en
+    else:
+        thousands_arr = thousands        
     if th > 1:
-        return "{} {} {}".format(num_to_text_hundreds(th, True),
-                                 thousands[1],
-                                 num_to_text_hundreds(r, False))
+        return "{0} {1} {2}".format(num_to_text_hundreds(th, True, english),
+                                    thousands_arr[1],
+                                    num_to_text_hundreds(r, False, english))
     elif th == 1:
-        return "{} {}".format(thousands[0], num_to_text_hundreds(r, False))
+        return "{0} {1}".format(thousands_arr[0],
+                                num_to_text_hundreds(r, False, english))
     else:
-        return num_to_text_hundreds(r, False)
+        return num_to_text_hundreds(r, False, english)
 
-def num_to_text_millions(number):
+def num_to_text_millions(number, english=False):
     m, r = divmod(number, 1000000)
-    if m > 1:
-        return "{} {} {}".format(num_to_text_hundreds(m, False),
-                              millions[1],
-                              num_to_text_thousands(r))
-    elif m == 1:
-        return "{} {} {}".format(one_to_twenty_n[0],
-                                 millions[0],
-                                 num_to_text_thousands(r))
+    if english:
+        millions_arr = millions_en
     else:
-        return num_to_text_thousands(number)
+        millions_arr = millions    
+    if m > 1:
+        return "{0} {1} {2}".format(num_to_text_hundreds(m, False),
+                                    millions_arr[1],
+                                    num_to_text_thousands(r, english))
+    elif m == 1:
+        return "{0} {1} {2}".format(one_to_twenty_n[0],
+                                    millions_arr[0],
+                                    num_to_text_thousands(r, english))
+    else:
+        return num_to_text_thousands(number, english)
 
-def num_to_text_billions(number):
+def num_to_text_billions(number, english=False):
     m, r = divmod(number, 1000000000)
-    if m > 1:
-        return "{} {} {}".format(num_to_text_hundreds(m, False),
-                                 billions[1],
-                                 num_to_text_millions(r))
-    elif m == 1:
-        return "{} {} {}".format(one_to_twenty_n[0],
-                                 billions[0],
-                                 num_to_text_millions(r))
+    if english:
+        billions_arr = billions_en
     else:
-        return num_to_text_millions(number)
+        billions_arr = billions
+    if m > 1:
+        return "{0} {1} {2}".format(num_to_text_hundreds(m, False),
+                                    billions_arr[1],
+                                    num_to_text_millions(r, english))
+    elif m == 1:
+        return "{0} {1} {2}".format(one_to_twenty_n[0],
+                                    billions_arr[0],
+                                    num_to_text_millions(r, english))
+    else:
+        return num_to_text_millions(number, english)
     
-def num_to_text(number):
-    return num_to_text_billions(number)
+def num_to_text(number, english=False):
+    return num_to_text_billions(number, english)
 
 
 parser = argparse.ArgumentParser(description='Invoice creator')
 parser.add_argument('invoice_data')
 parser.add_argument('-t', '--template', dest='template',
                     default='invoice.tex')
+parser.add_argument('-e', '--english', dest='english',
+                    action='store_true',
+                    default=False)
 
 args = parser.parse_args()
 
@@ -211,20 +292,43 @@ if vat_rate_prc.endswith('.00'):
 value = "{:.2f}".format(value_f)
 tax_f = value_f * tax_rate_f
 vat_element = root.find('vat')
-vat_f = value_f * vat_rate_f
+if vat_element is not None:
+    vat_f = float(vat_element.text)
+else:
+    vat_f = value_f * vat_rate_f
 total_f = value_f + vat_f
 tax = "{:.2f}".format(tax_f)
 vat = "{:.2f}".format(vat_f)
 total = "{:.2f}".format(total_f) 
 (intpart, floatpart) = total.split('.')
 
-numbertext = "{} ευρώ".format(num_to_text(int(intpart)))
+numbertext = "{0} ευρώ".format(num_to_text(int(intpart)))
 
+if args.english:
+    numbertext_en = "{0} euros".format(num_to_text(int(intpart), True))
+else:
+    numbertext_en = ""
+    
 if floatpart != '' :
     floatpart_i = int(floatpart)
     if floatpart_i > 0:
-        numbertext = "{} και {} λεπτά".format(numbertext,
-                                              num_to_text(int(floatpart)))
+        if floatpart_i > 1:
+            dec_desc = 'λεπτά'
+        else:
+            dec_desc = 'λεπτό'
+        numbertext = "{0} και {1} {2}".format(numbertext,
+                                              num_to_text(int(floatpart)),
+                                              dec_desc)
+        if args.english:
+            if floatpart_i > 1:
+                dec_desc = 'cents'
+            else:
+                dec_desc = 'cent'            
+            numbertext_en = "{0} and {1} {2}".format(numbertext_en,
+                                                     num_to_text(
+                                                         int(floatpart),
+                                                         True),
+                                                     dec_desc)
 
 outfn = 'invoice_' + num + '.tex'
   
@@ -248,5 +352,8 @@ with codecs.open(args.template, mode='r', encoding='utf-8') as inf:
             line = line.replace("{{TOTAL}}", total)
             line = line.replace("{{NUMBERTEXT}}",
                                 numbertext.decode('utf-8').capitalize())
+            line = line.replace("{{NUMBERTEXTEN}}",
+                                numbertext_en.decode('utf-8').capitalize())
+                
             outf.write(line)
 
